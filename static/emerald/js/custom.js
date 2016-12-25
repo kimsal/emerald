af501c1fc5607c1ee2be2dba8ff297c4d84956f0
user0 <kimsalsan007@gmail.com>
2016-12-25 10:44:01 +0700

    ew

--- static/emerald/js/custom.js
+++ static/emerald/js/custom.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
-  	// autoPlay: 3000, //Set AutoPlay to 3 seconds
     items : 4,
     lazyLoad : true,
+    navigation : true
   }); 
  
 });
-
-
-
